69508e7225881c5203a61d4a13df48d334b36d34
Mohd-Elham <muhammedelham@gmail.com>
2025-12-24 12:58:52 +0530

    dev

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args) {
-    final String branch = "MAIN";
+    final String branch = "DEV";
     System.out.printf(" THIS IS FROM %s BRANCH", branch);
   }
 }
