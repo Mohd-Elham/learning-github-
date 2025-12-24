4137bc83a5edbbe237d5f32147549c7578a932f4
Mohd-Elham <muhammedelham@gmail.com>
2025-12-24 13:06:04 +0530

    dev v2

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 public class Main{
+
+  public static int sub (int a, int b) {
+    return a - b;
+  }
   public static void main(String[] args) {
     final String branch = "DEV";
     System.out.printf(" THIS IS FROM %s BRANCH", branch);
