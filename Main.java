08378edb620603edba5a0401fd8ece5d54b7d338
Mohd-Elham <muhammedelham@gmail.com>
2025-12-24 13:04:21 +0530

    qa v2

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 public class Main{
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
   public static void main(String[] args) {
     final String branch = "QA";
     System.out.printf(" THIS IS FROM %s BRANCH", branch);
